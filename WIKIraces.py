6cafa02c5e021ee9c06313332e42728479f97d1a
actorpus <47696241+actorpus@users.noreply.github.com>
2023-04-19 23:50:26 +0100

    yep

--- WIKIraces.py
+++ WIKIraces.py
@@ -32,25 +32,30 @@ def wait_for_change(driver, from_url):
         return new_page
 
 
+def render(heading, content):
+    return f'data:text/html,<div style="padding: 32px; border: black 1px solid; position: absolute; left: 50%; ' \
+           f'top: 50%; transform: translate(-50%, -50%)"><h1>WIKI races</h1><h2>{heading}</h2><p>{content}</p></div>'
+
+
 def main():
     binary = FirefoxBinary(r"C:\Program Files\Mozilla Firefox\firefox.exe")
 
     driver = webdriver.Firefox(firefox_binary=binary)
 
     default = """
-    <div style="padding: 32px; border: black 1px solid; position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%)">
-        <h1>WIKI races</h1>
-        <form style="align-content: center" action="http://local/">
-            <label for="server">Server IP:</label>
-            <input type="text" name="server" id="server" placeholder="127.0.0.1" value="127.0.0.1"><br>
-            <label for="port">Server Port:</label>
-            <input type="text" name="port" id="port" placeholder="37126" value="37126"><br>
-            <label for="name">Name:</label>
-            <input type="text" name="name" id="name" placeholder="Joe">
-            <button type="submit">Submit</button>
-        </form>
-    </div>
-    """.replace("\r\n", "").replace("\n", "").replace("    ", "")
+<div style="padding: 32px; border: black 1px solid; position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%)">
+    <h1>WIKI races</h1>
+    <form style="align-content: center" action="http://local/">
+        <label for="server">Server IP:</label>
+        <input type="text" name="server" id="server" placeholder="192.168.56.7" value="192.168."><br>
+        <label for="port">Server Port:</label>
+        <input type="text" name="port" id="port" placeholder="37126" value="37126"><br>
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name" placeholder="Joe"><br>
+        <button type="submit">Submit</button>
+    </form>
+</div>
+""".replace("\r\n", "").replace("\n", "").replace("    ", "")
 
     page = f"data:text/html,{default}"
     driver.get(page)
@@ -60,7 +65,7 @@ def main():
 
     server, port, name = [_.split("=")[1] for _ in new_page.split("http://local/?")[1].split("&")]
 
-    page = f"data:text/html,<h1>Please wait for the server to acknowledge your existence</h1><a>connecting to {server}:{port}</a>"
+    page = render("Please wait for the server to acknowledge your existence", f"connecting to {server}:{port}")
     driver.get(page)
 
     print("information from the client... server", server, "port", port)
@@ -68,14 +73,13 @@ def main():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((server, int(port)))
 
-    time.sleep(1)
+    time.sleep(2)
 
     welcome_message = {
         "name": name,
     }
 
     client_socket.send(json.dumps(welcome_message).encode())
-
 
     data = ""
     while not data:
@@ -87,9 +91,8 @@ def main():
         driver.close()
         sys.exit(-1)
 
-    page = "data:text/html,<h1>waiting for game to start</h1>"
+    page = render("Waiting for game to start", "When the game starts you will be shown a preview of your target<br>Then as soon as the first page loads your off!")
     driver.get(page)
-
 
     data = ""
     while not data:
@@ -100,36 +103,51 @@ def main():
     start = data["start_point"]
     end = data["end_point"]
 
-    page = f"data:text/html,<h1>loading endpoint preview</h1>"
+    page = render("Loading target preview...", "Navigating around this page is pointless")
     driver.get(page)
-    time.sleep(5)
+    time.sleep(2)
 
     page = f"https://en.wikipedia.org/wiki/Special:Search?search={end['article'].replace(' ', '_')}"
     driver.get(page)
-    time.sleep(5)
+    time.sleep(10)
 
     end = driver.current_url
-    page = "data:text/html,<h1>Prepare to start the game!</h1>"
+    page = render("Prepare to start the game", "it will begin shortly")
     driver.get(page)
-    time.sleep(5)
+    time.sleep(2)
 
     page = f"https://en.wikipedia.org/wiki/Special:Search?search={start['article'].replace(' ', '_')}"
     driver.get(page)
 
+    time.sleep(1)
+    start = driver.current_url
+
     current = wait_for_change(driver, page)
     page = driver.current_url
-    print(page)
+
+    path = [page]
 
     while current != end:
         current = wait_for_change(driver, page)
         page = driver.current_url
-        print(page)
 
-    driver.get("data:text/html,<h1>You Win</h1>")
+        path.append(page)
+
+        if "://en.wikipedia.org/wiki" not in page:
+            page = start
+            driver.get(page)
+
+    # driver.get("data:text/html,<h1>You Win</h1>")
+    page = render("Congratulations! You win", "let us just inform the others...")
+    driver.get(page)
 
     client_socket.send(b"WIN")
+    time.sleep(1)
+    client_socket.send(json.dumps(path).encode())
 
     time.sleep(5)
+
+    driver.close()
 
 
 print("Checking client hash...")
@@ -140,6 +158,8 @@ if hsh != phsh:
     req = requests.get("https://raw.githubusercontent.com/actorpus/WIKIraces/main/WIKIraces.py")
     with open(__file__, "w") as file: file.write(req.text)
     sys.exit()
+else:
+    print("Client chilling")
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -12,11 +12,19 @@ class Client(threading.Thread):
         self.name = "DEFAULT NAME"
         self.has_won = False
         self.running = True
+        self.path = []
+        self.daemon = True
 
     def start_message(self, start, end):
         self.sock.send(json.dumps({
             "start_point": start,
             "end_point": end
+        }).encode())
+
+    def broadcast_win(self, winning_path):
+        self.sock.send(json.dumps({
+            "gameover": 1,
+            "path": winning_path
         }).encode())
 
     def run(self) -> None:
@@ -39,57 +47,72 @@ class Client(threading.Thread):
                 data = self.sock.recv(1024)
 
             if data == b'WIN':
+                data = b""
+                while not data:
+                    data = self.sock.recv(16_384)
+
+                self.path = json.loads(data.decode())
+
                 self.has_won = True
+                self.running = False
                 continue
 
             print(data)
 
 
+ip, port = socket.gethostbyname(socket.gethostname()), 37126
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_sock.bind(("127.0.0.1", 37126))
+server_sock.bind((ip, port))
 server_sock.listen(8)
+print(f"server running on {ip}:{port}")
 
 
-clients = []
+def run_server(players: int):
+    clients = []
 
+    for i in range(players):
+        connection, client = server_sock.accept()
 
-for _ in range(1):
-    connection, client = server_sock.accept()
+        print(f"{i + 1}/{players} new connection from", client)
 
-    print("new connection from", client)
+        c = Client(connection)
+        c.start()
 
-    c = Client(connection)
-    c.start()
+        clients.append(c)
 
-    clients.append(c)
+    print("all clients connected, building suspense...")
+    time.sleep(10)
 
+    with open("topviews-2022.json", "rb") as file:
+        top_views = file.read()
+        top_views = json.loads(top_views)
 
-time.sleep(10)
+    start = random.choice(top_views)
+    end = random.choice(top_views)
 
-with open("topviews-2022.json", "rb") as file:
-    top_views = file.read()
-    top_views = json.loads(top_views)
+    print("starting with")
+    print(start["article"])
+    print("to")
+    print(end["article"])
 
-start = random.choice(top_views)
-end = random.choice(top_views)
-
-print("starting with")
-print(start)
-print("to")
-print(end)
-
-for con in clients:
-    con.start_message(start, end)
-
-print("waiting for winner to transmit")
-
-won = None
-while won is None:
     for con in clients:
-        if con.has_won:
-            won = con
+        con.start_message(start, end)
 
-print("winner!", won.name)
+    print("waiting for winner to transmit")
 
-for con in clients:
-    con.running = False
+    won = None
+    while won is None:
+        for con in clients:
+            if con.has_won:
+                won = con
+
+    print("winner!", won.name, won.path)
+
+    for con in clients:
+        con.broadcast_win(won.path)
+        con.running = False
+
+
+while True:
+    server_for = int(input("server for? \n> "))
+    run_server(server_for)
