5523183d45ef19b2c4053d2da320d5ea6495365c
actorpus <47696241+actorpus@users.noreply.github.com>
2023-04-19 22:55:42 +0100

    Update WIKIraces.py

--- WIKIraces.py
+++ WIKIraces.py
@@ -134,9 +134,9 @@ def main():
 
 print("Checking client hash...")
 with open(__file__, "rb") as file: hsh = hashlib.sha1(file.read()).hexdigest()
-phsh = requests.get("https://raw.githubusercontent.com/actorpus/WIKIraces/main/client_hash").text
+phsh = requests.get("https://raw.githubusercontent.com/actorpus/WIKIraces/main/client_hash").text.strip()
 if hsh != phsh:
-    print(f"Bad Client Hash, {hsh=}")
+    print(f"Bad Client Hash, {hsh=} {phsh=}")
     req = requests.get("https://raw.githubusercontent.com/actorpus/WIKIraces/main/WIKIraces.py")
     with open(__file__, "w") as file: file.write(req.text)
     sys.exit()
